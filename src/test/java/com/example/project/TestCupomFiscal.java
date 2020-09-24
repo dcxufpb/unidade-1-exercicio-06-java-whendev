630d19feb74897244808df57947c87ac71446e84
whendev <whendel.morais.9@gmail.com>
2020-09-24 15:19:17 -0300

    exercise solved

--- src/test/java/com/example/project/TestCupomFiscal.java
+++ src/test/java/com/example/project/TestCupomFiscal.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class TestCupomFiscal {
 
 	private String BREAK = System.lineSeparator();
-	
+
 	private String TEXTO_ESPERADO_LOJA_COMPLETA = "Loja 1" + BREAK +
 			"Log 1, 10 C1" + BREAK +
 			"Bai 1 - Mun 1 - E1" + BREAK +
@@ -66,7 +66,7 @@ public class TestCupomFiscal {
 			"IE: 123456789" + BREAK;
 
 	@BeforeAll
-	public void setup() {
+	public static void setup() {
 		CupomFiscal.NOME_LOJA = "Loja 1";
 		CupomFiscal.LOGRADOURO = "Log 1";
 		CupomFiscal.NUMERO = 10;
@@ -90,21 +90,21 @@ public class TestCupomFiscal {
 	public void nomeVazio() {
 		CupomFiscal.NOME_LOJA = "";
 		verificarCampoObrigatorio("O campo nome da loja é obrigatório");
-		CupomFiscal.NOME_LOJA = "Arcos Dourados Com. de Alimentos LTDA";
+		CupomFiscal.NOME_LOJA = "Loja 1";
 	}
 
 	@Test
 	public void logradouroVazio() {
 		CupomFiscal.LOGRADOURO = "";
 		verificarCampoObrigatorio("O campo logradouro do endereço é obrigatório");
-		CupomFiscal.LOGRADOURO = "Av. Projetada Leste";
+		CupomFiscal.LOGRADOURO = "Log 1";
 	}
 
 	@Test
 	public void numeroZero() {
 		CupomFiscal.NUMERO = 0;
 		rodarTestarRetorno(TEXTO_ESPERADO_SEM_NUMERO);
-		CupomFiscal.NUMERO = 500;
+		CupomFiscal.NUMERO = 10;
 	}
 
 	@Test
@@ -125,69 +125,73 @@ public class TestCupomFiscal {
 	public void municipioVazio() {
 		CupomFiscal.MUNICIPIO = "";
 		verificarCampoObrigatorio("O campo município do endereço é obrigatório");
-		CupomFiscal.MUNICIPIO = "Campinas";
+		CupomFiscal.MUNICIPIO = "Mun 1";
 	}
 
 	@Test
 	public void estadoVazio() {
 		CupomFiscal.ESTADO = "";
 		verificarCampoObrigatorio("O campo estado do endereço é obrigatório");
-	    CupomFiscal.ESTADO = "SP";
+		CupomFiscal.ESTADO = "E1";
 	}
 
 	@Test
 	public void cepVazio() {
 		CupomFiscal.CEP = "";
 		rodarTestarRetorno(TEXTO_ESPERADO_SEM_CEP);
-	    CupomFiscal.CEP = "11111-111";
+		CupomFiscal.CEP = "11111-111";
 	}
 
 	@Test
 	public void telefoneVazio() {
 		CupomFiscal.TELEFONE = "";
 		rodarTestarRetorno(TEXTO_ESPERADO_SEM_TELEFONE);
-	    CupomFiscal.TELEFONE = "(11) 1111-1111";
+		CupomFiscal.TELEFONE = "(11) 1111-1111";
 	}
 
 	@Test
 	public void observacaoVazia() {
 		CupomFiscal.OBSERVACAO = "";
 		rodarTestarRetorno(TEXTO_ESPERADO_SEM_OBSERVACAO);
-	    CupomFiscal.OBSERVACAO = "Obs 1";
+		CupomFiscal.OBSERVACAO = "Obs 1";
 	}
 
 	@Test
 	public void cnpjVazio() {
 		CupomFiscal.CNPJ = "";
 		verificarCampoObrigatorio("O campo CNPJ da loja é obrigatório");
-	    CupomFiscal.CNPJ = "42.591.651/0797-34";
+		CupomFiscal.CNPJ = "11.111.111/1111-11";
 	}
 
 	@Test
 	public void inscricaoEstadualVazia() {
 		CupomFiscal.INSCRICAO_ESTADUAL = "";
 		verificarCampoObrigatorio("O campo inscrição estadual da loja é obrigatório");
-		CupomFiscal.INSCRICAO_ESTADUAL = "244.898.500.113";
+		CupomFiscal.INSCRICAO_ESTADUAL = "123456789";
 	}
-	
+
 	@Test
 	public void exercicio02_Customizado() {
-		//Defina seus próprios valores para as variáveis a seguir 
-		CupomFiscal.NOME_LOJA = "";
-		CupomFiscal.LOGRADOURO = "";
-		CupomFiscal.NUMERO = 0;
+		CupomFiscal.NOME_LOJA = "LOJAS AMERICANAS S.A.";
+		CupomFiscal.LOGRADOURO = "R SACADURA CABRAL";
+		CupomFiscal.NUMERO = 102;
 		CupomFiscal.COMPLEMENTO = "";
-		CupomFiscal.BAIRRO = "";
-		CupomFiscal.MUNICIPIO = "";
-		CupomFiscal.ESTADO = "";
-		CupomFiscal.CEP = "";
-		CupomFiscal.TELEFONE = "";
-		CupomFiscal.OBSERVACAO = "";
-		CupomFiscal.CNPJ = "";
-		CupomFiscal.INSCRICAO_ESTADUAL = "";
-		
-		//E atualize o texto esperado abaixo
-		rodarTestarRetorno("" + BREAK);
+		CupomFiscal.BAIRRO = "GAMBOA";
+		CupomFiscal.MUNICIPIO = "RIO DE JANEIRO";
+		CupomFiscal.ESTADO = "RJ";
+		CupomFiscal.CEP = "20.221-160";
+		CupomFiscal.TELEFONE = "(21) 2206-6708";
+		CupomFiscal.OBSERVACAO = "47.11-3-02 Comercio varejista de mercadorias em geral";
+		CupomFiscal.CNPJ = "33.014.556/0001-96";
+		CupomFiscal.INSCRICAO_ESTADUAL = "85.687.08-5";
+
+		rodarTestarRetorno("LOJAS AMERICANAS S.A." + BREAK +
+				"R SACADURA CABRAL, 102" + BREAK +
+				"GAMBOA - RIO DE JANEIRO - RJ" + BREAK +
+				"CEP:20.221-160 Tel (21) 2206-6708" + BREAK +
+				"47.11-3-02 Comercio varejista de mercadorias em geral" + BREAK +
+				"CNPJ: 33.014.556/0001-96" + BREAK +
+				"IE: 85.687.08-5" + BREAK);
 	}
 
 	private void rodarTestarRetorno(String expected) {
@@ -198,7 +202,7 @@ public class TestCupomFiscal {
 		// assertion
 		assertEquals(expected, retorno);
 	}
-	
+
 	private void verificarCampoObrigatorio(String mensagemEsperada) {
 		try {
 			CupomFiscal.dadosLoja();
@@ -206,5 +210,4 @@ public class TestCupomFiscal {
 			assertEquals(mensagemEsperada, e.getMessage());
 		}
 	}
-	
 }
